87d35cc7cfba915fadb8a2af6dd762890c648850
Katarina Zailac <katarina.zailac@gmail.com>
2024-03-18 09:10:40 +0100

    Move function to the only place it is used

--- modules/utils.py
+++ modules/utils.py
@@ -1,14 +1,3 @@
-import os
-
-
-def does_file_exist(filename):
-    if os.path.exists(filename) and os.path.isfile(filename):
-        return True
-
-    else:
-        return False
-
-
 class WarnException(Exception):
     def __init__(self, msg):
         self.msg = msg
